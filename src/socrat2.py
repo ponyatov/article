7366651846fa5d4535438eca2bf389a8f3548199
Dmitry Ponyatov <dponyatov@gmail.com>
2017-09-18 18:04:21 +0400

    + yield/Variable
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/socrat2.py
+++ src/socrat2.py
@@ -1,7 +1,17 @@
-def human():
-    yield 'socrates'
-    yield "Chelsea"
-    yield "Hillary"
-    yield "Bill"
+class Variable:
+    def __lshift__(self,o):
+        ' override << operator '
+        self.V = o ; return self
+    def __repr__(self):
+        ' override printable representation '
+        return '<%s>'%self.V
+
+def human(V):
+    yield V << 'socrates'
+    yield V << "Chelsea"
+    yield V << "Hillary"
+    yield V << "Bill"
     
-for i in human(): print i
+V = Variable()
+# equivalent to human(V) query
+for i in human(V): print V
